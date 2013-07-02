62a8ff26a66b56d4833d6dddb548b14d0bbc5f33
Joao Parreira <joaocardiff@gmail.com>
2013-07-02 13:33:44 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnrails::Application.config.secret_token = '0ab35896b1b7acf48677e0c7c5dc7cbe0e23c225163024498b48d433225343e2ca9ab30706f9b80667ced8533421085f8884b0bfed769cbce2e8bc884a235f46'
